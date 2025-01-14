758aa4dde39056e1d76a6a31e78ff70f3ab4a6b1
alexstrnik <alex.str.nik@gmail.com>
2025-01-14 17:12:47 +0300

    Initial Commit

---
+++ SenseOS/SenseOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenseOSApp.swift
+//  SenseOS
+//
+//  Created by Aleksandr Strizhnev on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SenseOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
